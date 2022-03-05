80de835525ac06d94146089108061645edf4b988
phamtanphat <phatdroid94@gmail.com>
2022-03-05 21:12:54 +0700

    ui

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -3,50 +3,110 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-class CalculatorPage extends StatefulWidget {
 
+class CalculatorPage extends StatefulWidget {
   @override
   State<CalculatorPage> createState() => _CalculatorPageState();
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
   final TextEditingController numberOneController = TextEditingController();
+  double? result;
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Calculator Page"),
       ),
       body: Container(
+        constraints: BoxConstraints.expand(),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: numberOneController,
-              onChanged: (text){
-
-              },
-              decoration: InputDecoration(
-                hintText: "Input number 1",
-                border: OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  onPressed: (){
-                    String text = numberOneController.text.toString();
-                    print(text);
-                  },
-                  icon: Icon(Icons.remove_red_eye_rounded),
-                )
-                // fillColor: Colors.grey,
-                // filled: true,
+            Text(
+              "May tinh bo tui",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: width / 20,
+                fontWeight: FontWeight.bold,
               ),
-              keyboardType: TextInputType.number,
-              // obscureText: true,
-              // cursorColor: Colors.red,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Input number 1",
+                  border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Input number 2",
+                    border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            showResult(),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: (){
 
+                          },
+                          child: Text("+")),
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text("-"))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text("*")),
+                      ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text("/"))
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  Widget showResult(){
+    if(result != null){
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(" = 10"),
+      );
+    }else{
+      return Container();
+    }
   }
 }
