722cb65046ad6426ff6c0a9d0b329b0ac41098e2
phamtanphat <phatdroid94@gmail.com>
2022-03-10 21:08:03 +0700

    dialog delete

--- lib/demo_listview.dart
+++ lib/demo_listview.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget19022022/model/todo_model.dart';
@@ -54,7 +55,7 @@ class _DemoListViewState extends State<DemoListView> {
         trailing: IconButton(
           icon: Icon(Icons.delete , color: Colors.red,),
           onPressed: (){
-
+            showDeleteDialog(context ,toDoModel);
           },
         ),
       ),
@@ -68,59 +69,61 @@ class _DemoListViewState extends State<DemoListView> {
         context: context,
         builder: (context){
           return Dialog(
-            child: Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
-                    child: Text("Add New Work" , style: TextStyle(fontWeight: FontWeight.bold , fontSize: 20),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
-                    child: TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Title"
-                      ),
-                      maxLines: 1,
+            child: SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
+                      child: Text("Add New Work" , style: TextStyle(fontWeight: FontWeight.bold , fontSize: 20),),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
-                    child: TextField(
-                      controller: descriptionController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "Description"
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
+                      child: TextField(
+                        controller: titleController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Title"
+                        ),
+                        maxLines: 1,
                       ),
-                      maxLines: 5,
                     ),
-                  ),
-                  Container(
-                      margin: EdgeInsets.only(top: 10),
-                      height: MediaQuery.of(context).size.height / 18,
-                      color: Colors.teal,
-                      child: ElevatedButton(
-                        onPressed: (){
-                          String title = titleController.text.toString();
-                          String description = descriptionController.text.toString();
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10 ,horizontal: 5),
+                      child: TextField(
+                        controller: descriptionController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Description"
+                        ),
+                        maxLines: 5,
+                      ),
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 10),
+                        height: MediaQuery.of(context).size.height / 18,
+                        color: Colors.teal,
+                        child: ElevatedButton(
+                          onPressed: (){
+                            String title = titleController.text.toString();
+                            String description = descriptionController.text.toString();
 
-                          if(title.isEmpty || description.isEmpty){
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Title or description is empty")));
-                            return;
-                          }
-                          setState(() {
-                            list?.add(ToDoModel(title: title, description: description));
-                            Navigator.pop(context);
-                          });
-                        },
-                        child: Text("Save"),
-                      )
-                  )
-                ],
+                            if(title.isEmpty || description.isEmpty){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Title or description is empty")));
+                              return;
+                            }
+                            setState(() {
+                              list?.add(ToDoModel(title: title, description: description));
+                              Navigator.pop(context);
+                            });
+                          },
+                          child: Text("Save"),
+                        )
+                    )
+                  ],
+                ),
               ),
             ),
           );
@@ -128,4 +131,43 @@ class _DemoListViewState extends State<DemoListView> {
     );
   }
 
+  void showDeleteDialog(BuildContext context , ToDoModel model){
+    if(Platform.isAndroid){
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return AlertDialog(
+            title: Text('Alert'),
+            content: Text('Are you sure delete item?'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('ok'),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                },
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+    showCupertinoDialog(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return CupertinoAlertDialog(
+            title: Text('Alert'),
+            content: Text('Are you sure delete item?'),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                },
+              ),
+            ],
+          );
+        },
+    );
+  }
 }
